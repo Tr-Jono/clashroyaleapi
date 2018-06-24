b1caee952100fb46c9c0553fbf0ad7c10c758dbe
Trainer Jono <37174208+Tr-Jono@users.noreply.github.com>
2018-06-24 03:27:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="clashroyaleapi",
     packages=["crapi"],
-    version="1.0",
+    version="1.1",
     license="GNU General Public License v3.0",
     author="Trainer Jono",
-    author_email="",
+    author_email="omgthisissouseless@gmail.com",
     url="https://github.com/Tr-Jono/clashroyaleapi",
     description="A RoyaleAPI Python wrapper."
 )
