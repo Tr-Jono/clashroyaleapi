e7d08e662ce67b4ee2e4f1b153ddbec42e86b157
Tr-Jono <yypsjonathanleung@gmail.com>
2018-12-21 23:09:12 +0800

    add stuff to constants

--- royaleapi/constants.py
+++ royaleapi/constants.py
@@ -1,6 +1,19 @@
 VALID_TAG_CHARS = "0289CGJLPQRUVY"
 
 
+class CardType:
+    TROOP = "Troop"
+    SPELL = "Spell"
+    BUILDING = "Building"
+
+
+class CardRarity:
+    COMMON = "Common"
+    RARE = "Rare"
+    EPIC = "Epic"
+    LEGENDARY = "Legendary"
+
+
 class ClanType:
     OPEN = "open"
     INVITE_ONLY = "invite only"
@@ -33,4 +46,36 @@ class TournamentState:
     ENDED = "ended"
 
 
-# TODO: Battle types
+# I have no clue what most of the following battle stuff means
+
+
+class BattleType:  # Battle.battle_type
+    FRIENDLY = "friendly"
+    CHALLENGE = "challenge"
+    CLANMATE = "clanMate"  # In-clan friendly battle
+    CLAN_WAR_WAR_DAY = "clanWarWarDay"
+    TWO_VS_TWO = "2v2"
+
+
+class BattleModeName:  # BattleMode.name
+    CHALLENGE = "Challenge"
+    CARD_RELEASE_DRAFT = "CardReleaseDraft"  # New card release draft challenge (e.g. ewiz draft)
+    SHOWDOWN_LADDER = "Showdown_Ladder"  # ???
+    TEAM_VS_TEAM_LADDER = "TeamVsTeamLadder"  # 2v2
+
+
+class DeckType:  # Battle.deck_type
+    COLLECTION = "Collection"  # From your own card collection
+    DRAFT = "Draft"
+    SLOT_DECK = "slotDeck"  # ???
+
+
+class CardLevelType:  # BattleMode.card_levels
+    FRIENDLY = "Friendly"  # Lv.9
+    LADDER = "Ladder"  # Lv.1 - Lv.13
+    TOURNAMENT = "Tournament"  # Lv.1 - Specified cap (usually Lv.9)
+
+
+class PlayerType:  # BattleMode.players
+    PVP = "PvP"  # 1v1
+    TVT = "TvT"  # 2v2
